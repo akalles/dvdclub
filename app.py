1268a04b1db17becc7ea5ec0dcb38260def31e56
leskal <alexakis1997@gmail.com>
2018-01-13 19:59:00 +0200

    Implement hash password for admin panel

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
+from flask_bcrypt import Bcrypt
+from flask.ext.admin.contrib import sqla
+from wtforms.fields import PasswordField
 import sqlite3 as sql
 
 import os
@@ -21,6 +24,7 @@ app.config['USER_PASSWORD_HASH'] = 'bcrypt'
 
 db = SQLAlchemy(app)
 admin = Admin(app)
+bcrypt = Bcrypt(app)
 
 class User(db.Model, UserMixin):
         id = db.Column(db.Integer, primary_key=True)
@@ -53,8 +57,35 @@ class Reservation(db.Model, UserMixin):
 db_adapter = SQLAlchemyAdapter(db, User)
 user_manager = UserManager(db_adapter, app)
 
-class UserView(ModelView):
-        form_columns=['username', 'password', 'active', 'admin']
+class UserView(sqla.ModelView):
+        form_columns=['username', 'active', 'admin']
+
+        # Don't include the standard password field when creating or editing a User (but see below)
+        form_excluded_columns = ('password')
+
+        # Automatically display human-readable names for the current and available Roles when creating or editing a User
+        column_auto_select_related = True
+
+        # On the form for creating or editing a User, don't display a field corresponding to the model's password field.
+        # There are two reasons for this. First, we want to encrypt the password before storing in the database. Second,
+        # we want to use a password field (with the input masked) rather than a regular text field.
+        def scaffold_form(self):
+            # Start with the standard form as provided by Flask-Admin. We've already told Flask-Admin to exclude the
+            # password field from this form.
+            form_class = super(UserView, self).scaffold_form()
+
+            # Add a password field, naming it "password2" and labeling it "Password".
+            form_class.password2 = PasswordField('Password')
+            return form_class
+
+        # This callback executes when the user saves changes to a newly-created or edited User -- before the changes are
+        # committed to the database.
+        def on_model_change(self, form, model, is_created):
+            # If the password field isn't blank...
+            if len(model.password2):
+                # ... then encrypt the new password prior to storing it in the database. If the password field is blank,
+                # the existing password in the database will be retained.
+                model.password = bcrypt.generate_password_hash(model.password2)
 
 class MovieView(ModelView):
         form_columns=['name', 'genre']
